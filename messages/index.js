3e75be4b300203521b37bdead90a00ee28faf10b
Samuel Damilola Owadayo <odevservices@gmail.com>
2026-06-16 11:08:13 +0100

    feat(messages): add CreatorCard messages to exports module

--- messages/index.js
+++ messages/index.js
@@ -1,3 +1,7 @@
 const AuthenticationMessages = require('./authentication');
+const CreatorCardMessages = require('./creatorCards')
 
-module.exports = { AuthenticationMessages };
+module.exports = {
+    AuthenticationMessages,
+    CreatorCardMessages
+};
